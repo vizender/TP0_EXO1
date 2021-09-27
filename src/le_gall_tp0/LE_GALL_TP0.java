21eb2a521dd36eb22e9e22ebe8071bca1f35bae3
Le Gall Victor <victor@looping.org>
2021-09-27 12:53:52 +0200

    Exo1 V1

---
+++ src/le_gall_tp0/LE_GALL_TP0.java
@@ -0,0 +1,34 @@
+/*
+Exo 0
+LE GALL
+27/09/2021
+ */
+package le_gall_tp0;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author victorlegall
+ */
+public class LE_GALL_TP0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Bonjour");
+        int a;
+        a=1;
+        a=a+1;
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in); 
+        System.out.println("Bonjour, quel est votre prenom ?"); 
+        prenom = sc.nextLine();
+
+        System.out.println("Au Revoir");
+    }
+    
+}
